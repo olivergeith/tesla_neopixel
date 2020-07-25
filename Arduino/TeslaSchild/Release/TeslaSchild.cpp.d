41e2551c0ad68dfd0e75862eeac94edcc46f0ca9
Oliver <oliver.geith@gmx.net>
2020-07-25 14:06:51 +0200

    initial import

---
+++ Arduino/TeslaSchild/Release/TeslaSchild.cpp.d
@@ -0,0 +1,42 @@
+TeslaSchild.cpp.o: ../TeslaSchild.cpp ../TeslaSchild.h \
+ C:\dataP\ws_git\arduino-projects\libraries\Adafruit_NeoPixel/Adafruit_NeoPixel.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Arduino.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/binary.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/WCharacter.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/WString.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/HardwareSerial.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Stream.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Print.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Printable.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/USBAPI.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Arduino.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\variants\eightanaloginputs/pins_arduino.h \
+ C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\variants\eightanaloginputs/../standard/pins_arduino.h
+
+../TeslaSchild.h:
+
+C:\dataP\ws_git\arduino-projects\libraries\Adafruit_NeoPixel/Adafruit_NeoPixel.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Arduino.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/binary.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/WCharacter.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/WString.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/HardwareSerial.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Stream.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Print.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Printable.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/USBAPI.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\cores\arduino/Arduino.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\variants\eightanaloginputs/pins_arduino.h:
+
+C:\dataP\Tools\Eclipse\eclipseArduino\arduinoPlugin\packages\arduino\hardware\avr\1.6.9\variants\eightanaloginputs/../standard/pins_arduino.h:
